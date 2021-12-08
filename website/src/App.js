a719d9938bb12ecac0776cf3d326bed0ba146353
gabirel <gabriel.knies@epitech.eu>
2021-12-08 17:31:41 +0100

    [master]: adding debut of homepage / navbar

--- website/src/App.js
+++ website/src/App.js
@@ -1,23 +1,24 @@
 import logo from './logo.svg';
 import './App.css';
+import Home from './Pages/Home/Home';
+import { useState, useEffect } from 'react';
+import { Routes, Route, Link } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div id="dashboard-title">Dashboard</div>
+        <div className="container">
+          <Link className="navbar-brand" to={"/"}>Home</Link>
+        </div>
+      </nav>
+
+      <div className="main-container">
+        <Routes>
+          <Route exact path='/' element={<Home />} />
+        </Routes>
+      </div>
     </div>
   );
 }
