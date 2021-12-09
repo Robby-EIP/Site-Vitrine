a1cea27f7fd1936c199ffa1c683cf4c89b3805f0
gabirel <gabriel.knies@epitech.eu>
2021-12-09 11:11:52 +0100

    [master]: adding css

--- website/src/Pages/Home/Home.js
+++ website/src/Pages/Home/Home.js
@@ -14,11 +14,18 @@ function Home() {
         <h1>Home</h1>
         <p>You clicked {count} times</p>
         <button onClick={() => setCount(count + 1)}>Click me</button>
-        <div>
-          <p style={{ color: '#7AFFBF', fontSize: 52 }}>Easily discover robotics programming</p>
-          <p style={{ color: '#8D8282', fontSize: 24 }}>Robby is the solution</p>
+        <div style={{ width: '90%', paddingLeft: '5%', paddingTop: '4%', display: 'flex' }}>
+          <div>
+            <div style={{ width: '35%' }}>
+              <p style={{ color: '#7AFFBF', fontSize: 68 }}>Easily discover robotics programming</p>
+            </div>
+            <div style={{ width: '35%' }}>
+              <p style={{ color: '#8D8282', fontSize: 32 }}>Robby is the solution</p>
+            </div>
+
+          </div>
+          <img src="undraw_firmware_re_fgdy.svg" style={{ width: '30%' }}></img>
         </div>
-        <img src="undraw_firmware_re_fgdy.svg" style={{ width: '30%' }}></img>
       </div>
       <div style={{ backgroundColor: '#1B152D', height: '44.9%', width: '100%' }}></div>
       <div style={{ height: '0.1%', width: '100%' }}></div>
