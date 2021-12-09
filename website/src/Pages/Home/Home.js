8af228ce3a88b3685b8aae9bf414eef02d931fd1
gabirel <gabriel.knies@epitech.eu>
2021-12-09 14:20:53 +0100

    [master]: fixing css

--- website/src/Pages/Home/Home.js
+++ website/src/Pages/Home/Home.js
@@ -3,9 +3,9 @@ function Home() {
   return (
     <>
       <div style={{ backgroundColor: '#322653', height: '55%', width: '100%' }}>
-        <div style={{ width: '85%', paddingLeft: '15%', paddingTop: '14%', display: 'flex' }}>
+        <div style={{ width: '85%', paddingLeft: '15%', paddingTop: '10%', display: 'flex' }}>
           <div style={{ width: '20%' }}>
-            <div style={{ width: '130%' }}>
+            <div style={{ width: '130%', paddingTop: "20%" }}>
               <span style={{ color: '#7AFFBF', fontSize: 68 }}>Easily discover robotics programming</span>
             </div>
             <div style={{ width: '125%', paddingTop: '1.5%' }}>
@@ -13,8 +13,8 @@ function Home() {
             </div>
           </div>
           <div style={{ width: '50%', position: 'relative' }}>
-            <img src="Vector1.svg" style={{ width: '65%', position: 'absolute' }} />
-            <img src="undraw_firmware_re_fgdy.svg" style={{ width: '40%', position: 'absolute' }} />
+            <img src="Vector1.svg" style={{ width: '85%', position: 'absolute' }} />
+            <img src="undraw_firmware_re_fgdy.svg" style={{ width: '92%', position: 'absolute' }} />
           </div>
         </div>
       </div>
