0efedd103ec511efb1ace211733b6f916d8ff033
gabirel <gabriel.knies@epitech.eu>
2021-12-10 16:18:18 +0100

    [master]: adding stuff

--- website/src/Pages/Home/Home.js
+++ website/src/Pages/Home/Home.js
@@ -1,4 +1,24 @@
+import { useState, useEffect } from 'react';
+
 function Home() {
+  // const [images, setImages] = useState([]);  
+
+
+  // import all images from public/carousel
+  // useEffect(() => {
+  //   const images = require.context('./carousel/', true);
+  //   console.log("test ", images);
+  //   console.log("test.keys() ", images.keys());
+  //   setImages(images.keys());
+  // }, []);
+
+  // const importAll = (r) => {
+  //   return r.keys().map(r);
+  // };
+
+  // useEffect(() => {
+  //   setImages(importAll(require.context('carousel/', false, /\.(png|jpe?g|svg)$/)));
+  // }, []);
 
   return (
     <>
@@ -13,8 +33,8 @@ function Home() {
             </div>
           </div>
           <div style={{ width: '50%', position: 'relative' }}>
-            <img src="Vector1.svg" style={{ width: '85%', position: 'absolute' }} />
-            <img src="undraw_firmware_re_fgdy.svg" style={{ width: '92%', position: 'absolute' }} />
+            <img src="Vector1.svg" alt='1' style={{ width: '85%', position: 'absolute' }} />
+            <img src="undraw_firmware_re_fgdy.svg" alt='code' style={{ width: '92%', position: 'absolute' }} />
           </div>
         </div>
       </div>
@@ -30,12 +50,12 @@ function Home() {
             <span style={{ color: 'white', fontSize: 28 }} >Robby is made for you !</span>
           </div>
           <div>
-            <img src="RobbyEIP.png" style={{ width: '15%' }} />
+            <img src="RobbyEIP.png" alt='' style={{ width: '15%' }}></img>
           </div>
         </div>
         <div style={{ color: 'white', width: '100%', height: '10%' }}>
-          <a href="https://github.com/Robby-EIP" target={"_blank"} style={{ marginRight: '30%' }}><img alt="our github" src="icons8-github.svg" style={{ width: '7%', minWidth: '75px' }} /></a>
-          <a href="https://discord.gg/Y5mjy86rFS" target={"_blank"}><img alt="our discord" src="icons8-discord-bubble.svg" style={{ width: '7%', minWidth: '75px' }} /></a>
+          <a href="https://github.com/Robby-EIP" rel="noreferrer" target={"_blank"} style={{ marginRight: '30%' }}><img alt="our github" src="icons8-github.svg" style={{ width: '7%', minWidth: '75px' }} /></a>
+          <a href="https://discord.gg/Y5mjy86rFS" rel="noreferrer" target={"_blank"}><img alt="our discord" src="icons8-discord-bubble.svg" style={{ width: '7%', minWidth: '75px' }} /></a>
         </div>
       </div>
       <div style={{ height: '0.1%', width: '100%' }}></div>
@@ -43,27 +63,27 @@ function Home() {
         <div style={{ paddingTop: '15%', width: '100%', display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
           <div style={{ height: '30%', width: '30%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
             <span style={{ color: 'white', fontSize: 32 }}>Funny Exercises</span>
-            <img src="icons8-google-code.svg" style={{ width: '25%', minWidth: '75px', paddingTop: '7%' }} />
+            <img src="icons8-google-code.svg" alt="code.svg" style={{ width: '25%', minWidth: '75px', paddingTop: '7%' }} />
           </div>
           <div style={{ height: '30%', width: '30%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
             <span style={{ color: 'white', fontSize: 32 }}>Code / Blocks</span>
-            <img src="If_then_else.svg" style={{ width: '25%', minWidth: '75px', paddingTop: '7%' }} />
+            <img src="If_then_else.svg" alt='code blocks' style={{ width: '25%', minWidth: '75px', paddingTop: '7%' }} />
           </div>
           <div style={{ height: '30%', width: '30%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
             <span style={{ color: 'white', fontSize: 32 }}>Turnkey Solution</span>
-            <img src="image 6.svg" style={{ width: '25%', minWidth: '75px', paddingTop: '7%' }} />
+            <img src="image 6.svg" alt='solution'  style={{ width: '25%', minWidth: '75px', paddingTop: '7%' }} />
           </div>
         </div>
         <div style={{ paddingTop: '7%', width: '100%', display: 'flex', flexDirection: 'row', justifyContent: 'center' }}>
           <div style={{ height: '30%', width: '30%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
             <span style={{ color: 'white', fontSize: 32 }}>Robots Smart Monitoring</span>
-            <img src="image 8.svg" style={{ width: '25%', minWidth: '75px', paddingTop: '7%' }} />
+            <img src="image 8.svg" alt="smart robot" style={{ width: '25%', minWidth: '75px', paddingTop: '7%' }} />
           </div>
           <div style={{ height: '30%', width: '15%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
           </div>
           <div style={{ height: '30%', width: '30%', display: 'flex', flexDirection: 'column', justifyContent: 'center', alignItems: 'center' }}>
             <span style={{ color: 'white', fontSize: 32 }}>Modular</span>
-            <img src="image 9.svg" style={{ width: '25%', minWidth: '75px', paddingTop: '7%' }} />
+            <img src="image 9.svg" alt='modular' style={{ width: '25%', minWidth: '75px', paddingTop: '7%' }} />
           </div>
         </div>
       </div>
@@ -76,7 +96,12 @@ function Home() {
       <div style={{ height: '0.1%', width: '100%' }}></div>
       <div style={{ backgroundColor: '#1B152D', height: '100%', width: '100%' }}>
         <div style={{ backgroundColor: 'red', paddingTop: '7%', width: '100%', display: 'flex', flexDirection: 'column' }}>
-          <span >page carrousel</span>
+          <span >page carousel</span>
+          {/* {images.map((image, index) => (
+            console.log('image == ', image),
+            <img key={index} src={image} alt="carousel" style={{ width: '100%', height: '100%' }} />
+          ))} */}
+          <img src='carousel/gabriel_knies.jpeg' alt="carousel" style={{ width: '10%' }} />
         </div>
       </div>
       <div style={{ height: '0.1%', width: '100%' }}></div>
